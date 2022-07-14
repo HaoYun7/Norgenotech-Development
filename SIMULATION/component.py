f0f03198a0f8fc805994e5d94c498e0b6e710494
HaoYun7 <hyun199834@gmail.com>
2022-07-14 15:18:23 +0200

    the graphical model of the robot is added

---
+++ SIMULATION/component.py
@@ -0,0 +1,71 @@
+from vpython import *
+
+
+class Component:
+    def __init__(self):
+        # parameters for ground
+        self.ground_location = vector(0, -5, 0)
+        self.ground_color = color.white
+        self.ground_length = 9
+        self.ground_height = .1
+        self.ground_width = 5
+
+        # parameters for right bar
+        self.right_bar_location = vector(0, -1, 2.25)
+        self.right_bar_color = color.yellow
+        self.right_bar_length = 9
+        self.right_bar_height = .5
+        self.right_bar_width = .5
+
+        # parameters for left bar
+        self.left_bar_location = vector(0, -1, -2.25)
+        self.left_bar_color = color.yellow
+        self.left_bar_length = 9
+        self.left_bar_height = .5
+        self.left_bar_width = .5
+
+        # parameter for mid bar
+        self.mid_bar_location = vector(0, -1, 0)
+        self.mid_bar_color = color.blue
+        self.mid_bar_length = .5
+        self.mid_bar_height = .5
+        self.mid_bar_width = 4
+
+        # parameter for hand
+        self.hand_location = vector(0, -1.5, 0)
+        self.hand_color = color.green
+        self.hand_length = 1
+        self.hand_height = 1.5
+        self.hand_width = 1
+
+        # parameter for finger
+        self.finger_location = vector(0, -2.0, 0)
+        self.finger_color = color.red
+        self.finger_radius = .5
+        self.finger_axis = vector(0, -1, 0)
+
+    def set_ground(self):
+        return box(pos=self.ground_location, color=self.ground_color, length=self.ground_length, height=self.ground_height, width=self.ground_width)
+
+    def set_right_bar(self):
+        return box(pos=self.right_bar_location, color=self.right_bar_color, length=self.right_bar_length, height=self.right_bar_height, width=self.right_bar_width)
+
+    def set_left_bar(self):
+        return box(pos=self.left_bar_location, color=self.left_bar_color, length=self.left_bar_length, height=self.left_bar_height, width=self.left_bar_width)
+
+    def set_mid_bar(self):
+        return box(pos=self.mid_bar_location, color=self.mid_bar_color, length=self.mid_bar_length, height=self.mid_bar_height, width=self.mid_bar_width)
+
+    def set_hand(self):
+        return box(pos=self.hand_location, color=self.hand_color, length=self.hand_length, height=self.hand_height, width=self.hand_width)
+
+    def set_finger(self):
+        return cone(pos=self.finger_location, color=self.finger_color, axis=self.finger_axis, radius=self.finger_radius)
+
+    def build_robot(self):
+        ground = self.set_ground()
+        right_bar = self.set_right_bar()
+        left_bar = self.set_left_bar()
+        mid_bar = self.set_mid_bar()
+        hand = self.set_hand()
+        finger = self.set_finger()
---
+++ SIMULATION/graphic.py
@@ -0,0 +1,9 @@
+from vpython import *
+import component
+
+robot = component.Component()
+
+robot.build_robot()
+
+while True:
+    pass
